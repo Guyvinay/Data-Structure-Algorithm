a2e01f5431a2e6ce4cfbaab7073fe8600ba65235
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-08-20 02:20:40 +0530

    container-with-most-water-with-two-pointer-and-brut-force

---
+++ Mathematics/Containerwithmostwater.java
@@ -0,0 +1,29 @@
+package Mathematics;
+
+class Solution {
+         public int maxArea(int[] height) {
+        // int max = 0;
+        // for(int i=0; i<height.length; i++){
+        //     for(int j=i+1; j<height.length; j++){
+        //         int area = Math.min(height[i], height[j])*(j-i);
+        //         max = Math.max(max , area);
+        //     }
+        // }
+        // return max;
+        int l = 0,
+            r = height.length-1,
+            max = 0;
+            while(l<r){
+                int area = Math.min(height[l],height[r])*(r-l);
+                max = Math.max(max , area);
+                if(height[l]<height[r])l++;
+                else r--;
+            }
+            return max;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = {21,3,8,9,4,5,7,8};
+        System.out.println(sol.maxArea(arr));
+    }
+}
