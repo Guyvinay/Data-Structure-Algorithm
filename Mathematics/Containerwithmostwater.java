14401900b5e69380db23a0d93fe726cdfde122ca
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-09-02 22:49:38 +0530

    container-with-most-water

--- Mathematics/Containerwithmostwater.java
+++ Mathematics/Containerwithmostwater.java
@@ -2,24 +2,25 @@ package Mathematics;
 
 class Solution {
          public int maxArea(int[] height) {
-        // int max = 0;
-        // for(int i=0; i<height.length; i++){
-        //     for(int j=i+1; j<height.length; j++){
-        //         int area = Math.min(height[i], height[j])*(j-i);
-        //         max = Math.max(max , area);
-        //     }
-        // }
-        // return max;
-        int l = 0,
-            r = height.length-1,
-            max = 0;
-            while(l<r){
-                int area = Math.min(height[l],height[r])*(r-l);
+        int max = 0;
+        for(int i=0; i<height.length; i++){
+            for(int j=i+1; j<height.length; j++){
+                int area = Math.min(height[i], height[j])*(j-i);
                 max = Math.max(max , area);
-                if(height[l]<height[r])l++;
-                else r--;
             }
-            return max;
+        }
+        return max;
+        // int l = 0,
+        //     r = height.length-1,
+        //     max = 0;
+        //     while(l<r){
+        //         int area = Math.min(height[l],height[r])*(r-l);
+        //         max = Math.max(max , area);
+        //         if(height[l]<height[r])l++;
+        //         else r--;
+        //     }
+        //     return max;
+        // System.out.println("Vinay is A Good Boy!");
     }
     public static void main(String[] args) {
         Solution sol = new Solution();
