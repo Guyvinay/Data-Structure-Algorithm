8f6eed48c183fcc2374bfa4b04c8ef62a58fddcc
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-09-04 23:41:47 +0530

    Revision-java-day-05-ass02

--- Stacks/NextGreaterElement.java
+++ Stacks/NextGreaterElement.java
@@ -5,16 +5,14 @@ import java.util.Stack;
 
 public class NextGreaterElement {
     public int[] nextGreaterElement(int[] arr){
-
         Stack<Integer> stk = new Stack<>();
         int[] ans = new int[arr.length];
-        StringBuilder sb = new StringBuilder();
         for(int i=arr.length-1; i>=0; i--){
             while(!stk.isEmpty() && stk.peek()<=arr[i])stk.pop();
             if(stk.isEmpty())ans[i]=-1;
             else ans[i]=stk.peek();
             stk.push(arr[i]);
-        }
+        } 
         return ans;
     }
     public static void main(String[] args) {
