bbe7e738939020c3d2c3a87f4d723c1fd150d189
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-09-09 02:14:10 +0530

    Oj-problems

---
+++ OJ-Probs/Mathematics/AToBPossible.java
@@ -0,0 +1,18 @@
+package Mathematics;
+
+public class AToBPossible {
+    public String aToBPossible(int a , int b){
+
+        while(b>a){
+            if(b%2==0)b=b/2;
+            else if(b%10==1) b = (b-1)/10;
+            else break;
+        }
+        return a==b?"Possible":"Not Possible";
+
+    }
+    public static void main(String[] args) {
+        AToBPossible ab = new AToBPossible();
+        System.err.println(ab.aToBPossible(10 , 30));
+    }
+}
