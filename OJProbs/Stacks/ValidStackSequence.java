007ae7262e6cdf9c8cbde6a6a3e6e2ee426766e4
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-09-11 20:16:58 +0530

    stack-questions

---
+++ OJProbs/Stacks/ValidStackSequence.java
@@ -0,0 +1,25 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class ValidStackSequence{
+
+    public boolean validStackSequence(int[] arr1 , int[] arr2){
+        int cnt=0;
+        Stack<Integer> stk = new Stack<>();
+        for(int i=0; i<arr1.length; i++){
+
+            
+            while(!stk.isEmpty() && cnt < arr1.length && stk.peek() == arr2[cnt] ){
+                stk.pop();
+                cnt++;
+            }stk.push(arr1[i]);
+        }
+        return cnt==arr2.length?true:false; 
+    }
+
+    public static void main(String[] args) {
+        ValidStackSequence vss = new ValidStackSequence();
+        System.out.println(vss.validStackSequence(new int[]{1,2,3,4,5}, new int[]{4,5,3,2,1}));
+    }
+}
