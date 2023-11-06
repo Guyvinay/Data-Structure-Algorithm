d4c39edc508a42ce0dfbaa3afc6ba35e5922881d
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-11-06 21:01:08 +0530

    Smaller-neighbour-element-py

---
+++ Python-ACC/OJ-Probs-py/SmallerNeighbourElement.py
@@ -0,0 +1,13 @@
+def smallerNeighbour(arr) :
+    stk = []
+    ans = [-1]*len(arr)
+    for i in range(len(arr)) :
+        while stk and stk[-1][0] >= arr[i]:
+            stk.pop()
+        if stk :
+            ans[i]=stk[-1][0]
+        stk.append((arr[i],i))    
+    return ans
+test = int(input())
+arr = list(map(int,input().split()))
+print(smallerNeighbour(arr))     
