e33ab02b41c1335289de3001a7bd1974a646e126
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-12-25 19:04:58 +0530

    Python-font

---
+++ ExtProb/PythonFont.py
@@ -0,0 +1,9 @@
+from colorama import Fore
+import pyfiglet
+font = pyfiglet.figlet_format("Vinay")
+print(Fore.RED+font)
+# __     _____ _   _    _ __   __
+# \ \   / /_ _| \ | |  / \\ \ / /
+#  \ \ / / | ||  \| | / _ \\ V /
+#   \ V /  | || |\  |/ ___ \| |
+#    \_/  |___|_| \_/_/   \_\_|
