130495b308a49c0c6301e6d02ffe36933327f302
guyvinay <mrsinghvinay563@gmial.com>
2025-08-13 08:59:30 +0530

    maxSubArray

--- dsa-problems/src/main/java/com/dsa/practice/MaximumSubarraySum.java
+++ dsa-problems/src/main/java/com/dsa/practice/MaximumSubarraySum.java
@@ -2,12 +2,17 @@ package com.dsa.practice;
 
 public class MaximumSubarraySum {
     public static void main(String[] args) {
-        int[] arr = {12,43, 56, 24, 1, 1};
+        int[] arr = {1, 0, 3, -8};
         System.out.println(maxSubArray(arr));
     }
+
     public static int maxSubArray(int[] nums) {
-
-        return -1;
+        int max = nums[0];
+        int curr = nums[0];
+        for(int i=1; i<nums.length; i++) {
+            curr = Math.max(nums[i], nums[i] + curr);
+            max = Math.max(curr, max);
+        }
+        return max;
     }
-
 }
--- dsa-problems/src/main/java/com/dsa/practice/e_commerce.sql
+++ dsa-problems/src/main/java/com/dsa/practice/e_commerce.sql
@@ -1,4 +1,4 @@
-CREATE TABLE customers (
+create TABLE customers (
     customer_id SERIAL PRIMARY KEY,
     customer_name VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE customers (
 ### **2. categories**
 
 ```sql
-CREATE TABLE categories (
+create TABLE categories (
     category_id SERIAL PRIMARY KEY,
     category_name VARCHAR(100) NOT NULL,
     description TEXT
@@ -27,7 +27,7 @@ CREATE TABLE categories (
 
 ```sql
 
-CREATE TABLE products (
+create TABLE products (
     product_id SERIAL PRIMARY KEY,
     product_name VARCHAR(150) NOT NULL,
     category_id INT REFERENCES categories(category_id),
@@ -44,7 +44,7 @@ CREATE TABLE products (
 
 ```sql
 
-CREATE TABLE suppliers (
+create TABLE suppliers (
     supplier_id SERIAL PRIMARY KEY,
     supplier_name VARCHAR(150) NOT NULL,
     contact_email VARCHAR(150),
@@ -59,7 +59,7 @@ CREATE TABLE suppliers (
 
 ```sql
 
-CREATE TABLE inventory (
+create TABLE inventory (
     product_id INT PRIMARY KEY REFERENCES products(product_id),
     quantity INT NOT NULL CHECK (quantity >= 0),
     last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -73,7 +73,7 @@ CREATE TABLE inventory (
 
 ```sql
 
-CREATE TABLE orders (
+create TABLE orders (
     order_id SERIAL PRIMARY KEY,
     customer_id INT REFERENCES customers(customer_id),
     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -89,7 +89,7 @@ CREATE TABLE orders (
 
 ```sql
 
-CREATE TABLE order_items (
+create TABLE order_items (
     order_item_id SERIAL PRIMARY KEY,
     order_id INT REFERENCES orders(order_id),
     product_id INT REFERENCES products(product_id),
@@ -105,7 +105,7 @@ CREATE TABLE order_items (
 
 ```sql
 
-CREATE TABLE payments (
+create TABLE payments (
     payment_id SERIAL PRIMARY KEY,
     order_id INT REFERENCES orders(order_id),
     payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -122,7 +122,7 @@ CREATE TABLE payments (
 
 ```sql
 
-CREATE TABLE reviews (
+create TABLE reviews (
     review_id SERIAL PRIMARY KEY,
     product_id INT REFERENCES products(product_id),
     customer_id INT REFERENCES customers(customer_id),
@@ -139,10 +139,10 @@ CREATE TABLE reviews (
 
 ```sql
 
-CREATE INDEX idx_orders_customer_id ON orders(customer_id);
-CREATE INDEX idx_order_items_product_id ON order_items(product_id);
-CREATE INDEX idx_products_category_id ON products(category_id);
-CREATE INDEX idx_payments_order_id ON payments(order_id);
+create index idx_orders_customer_id on orders(customer_id);
+create index idx_order_items_product_id on order_items(product_id);
+create index idx_products_category_id on products(category_id);
+create index idx_payments_order_id on payments(order_id);
 
 ```
 
@@ -152,75 +152,137 @@ CREATE INDEX idx_payments_order_id ON payments(order_id);
 
 ```sql
 
-INSERT INTO customers (customer_name, email, city, country)
-VALUES
+insert into customers (customer_name, email, city, country)
+values
 ('John Doe', 'john@example.com', 'New York', 'USA'),
 ('Jane Smith', 'jane@example.com', 'London', 'UK'),
 ('Amit Kumar', 'amit@example.com', 'Delhi', 'India');
 
-INSERT INTO categories (category_name) VALUES
+insert into categories (category_name) values
 ('Electronics'), ('Clothing'), ('Books');
 
-INSERT INTO products (product_name, category_id, price, supplier_id)
-VALUES
+insert into products (product_name, category_id, price, supplier_id)
+values
 ('Smartphone', 1, 699.99, 1),
 ('Laptop', 1, 1200.00, 2),
 ('T-Shirt', 2, 19.99, 3);
 
-INSERT INTO suppliers (supplier_name, contact_email)
-VALUES
+insert into suppliers (supplier_name, contact_email)
+values
 ('Tech Corp', 'sales@techcorp.com'),
 ('Gadgets Ltd', 'info@gadgets.com'),
 ('Cloth House', 'contact@clothhouse.com');
 
-INSERT INTO inventory (product_id, quantity)
-VALUES
+insert into inventory (product_id, quantity)
+values
 (1, 50),
 (2, 30),
 (3, 100);
 
-INSERT INTO orders (customer_id, status, total_amount)
-VALUES
+insert into orders (customer_id, status, total_amount)
+values
 (1, 'Delivered', 1399.99),
 (2, 'Pending', 699.99);
 
-INSERT INTO order_items (order_id, product_id, quantity, price)
-VALUES
+insert into order_items (order_id, product_id, quantity, price)
+values
 (1, 1, 1, 699.99),
 (1, 2, 1, 700.00),
 (2, 1, 1, 699.99);
 
-INSERT INTO payments (order_id, amount, payment_method, status)
-VALUES
+insert into payments (order_id, amount, payment_method, status)
+values
 (1, 1399.99, 'Credit Card', 'Completed'),
 (2, 699.99, 'UPI', 'Pending');
 
 ```
 
-select *
+select o.order_id, count(oi.order_item_id)
 from orders o
 join order_items oi on o.order_id = oi.order_id
 group by o.order_id;
 
-SELECT
+select
  *
-FROM orders o
-JOIN customers c ON o.customer_id = c.customer_id;
+from orders o
+join customers c on o.customer_id = c.customer_id;
 
-SELECT
+select
  *
-FROM products p
-LEFT JOIN categories c ON p.category_id = c.category_id;
+from products p
+left join categories c on p.category_id = c.category_id;
 
-SELECT
+select
  *
-FROM categories c
-LEFT JOIN products p  ON p.category_id = c.category_id;
+from categories c
+left join products p  on p.category_id = c.category_id;
 
-SELECT
+select
     p.product_id,
     p.product_name,
     c.category_name,
     p.price
-FROM products p
-JOIN categories c ON p.category_id = c.category_id;
+from products p
+join categories c on p.category_id = c.category_id;
+
+
+select * from
+products p right join suppliers s
+on p.supplier_id = s.supplier_id;
+
+select c.customer_id, c.customer_name, c.email, o.order_id
+from customers c
+join orders o
+on c.customer_id = o.customer_id
+where o.status = 'Pending';
+
+-- Create table: student
+create TABLE student (
+    roll_no INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100),
+    x_per NUMERIC(5, 2) NOT NULL CHECK (x_per >= 0 AND x_per <= 100),
+    state VARCHAR(50) NOT NULL
+);
+
+-- Insert data
+insert into student (roll_no, name, email, x_per, state) values
+(1, 'ABC', 'abc@ms.com', 77.65, 'Karnataka'),
+(2, 'BCD', 'bcd@ms.com', 33.00, 'Tamilnadu'),
+(3, 'CDE', 'cde@ms.com', 77.24, 'Maharashtra'),
+(4, 'def', 'def@ms.com', 74.69, 'West Bengal'),
+(5, 'efg', 'efg@ms.com', 66.00, 'Kerala'),
+(6, 'fgh', 'fgh@ms.com', 33.00, 'Delhi'),
+(7, 'ghi', 'ghi@ms.com', 33.00, 'Punjab'),
+(8, 'ijk', 'ijk@ms.com', 79.36, 'Haryana'),
+(10, 'AED', NULL, 56.00, 'Karnataka');
+
+
+select roll_no "Roll number", name "Name" from students;
+
+select name, x_per (
+    select avg(x_per) from student
+) from student;
+
+select roll_no "Roll number", name "Name" from students;
+
+select roll_no "Roll number", name "Name" from students;
+
+SELECT rollNo, x_per
+FROM (
+  SELECT rollNo, name,
+	x_per, email
+	FROM student
+	WHERE x_per > 60.00
+) T;
+
+SELECT * from student
+where x_per > (
+ select max(x_per) marks
+ from student where state = 'Karnataka'
+);
+
+SELECT state, AVG(x_per) from student group by state
+having avg(x_per) > (
+ select avg(x_per) from student where state = 'Karnataka'
+);
