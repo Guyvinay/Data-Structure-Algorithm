8fe0068a80dfb4308c8878f2695506cc04f69c12
guyvinay <mrsinghvinay563@gmial.com>
2025-11-09 22:17:50 +0530

    moveZeroes

--- dsa-problems/src/main/java/com/dsa/practice/arrays/ArraysProblem.java
+++ dsa-problems/src/main/java/com/dsa/practice/arrays/ArraysProblem.java
@@ -1,10 +1,23 @@
 package com.dsa.practice.arrays;
 
+import java.util.Arrays;
+
 public class ArraysProblem {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 8, 3, 7, 6, 4, 5, 6};
+        int[] arr = {10, 0, 2, 8, 0, 3, 0, 7, 6, 0, 4, 5, 6};
 //        reverse(arr);
-        System.out.println(secondLargest(arr));
+//        System.out.println(secondLargest(arr));
+        System.out.println(Arrays.toString(moveZeroesToEnd(arr)));
+    }
+
+    private static int[] moveZeroesToEnd(int[] arr) {
+
+        int ind = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) arr[ind++] = arr[i];
+        }
+        while (ind<arr.length) arr[ind++] = 0;
+        return arr;
     }
 
     private static int secondLargest(int[] arr) {
