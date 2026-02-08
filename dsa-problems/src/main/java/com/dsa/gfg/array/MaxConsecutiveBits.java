8ec29b26357b5b17891a3dd11e683885aa96bbd1
guyvinay <mrsinghvinay563@gmial.com>
2026-02-08 21:10:09 +0530

    maxConsecBitwise

--- dsa-problems/src/main/java/com/dsa/gfg/array/MaxConsecutiveBits.java
+++ dsa-problems/src/main/java/com/dsa/gfg/array/MaxConsecutiveBits.java
@@ -4,7 +4,7 @@ package com.dsa.gfg.array;
 public class MaxConsecutiveBits {
     public static void main(String[] args) {
         int[] arr = {1,1,0,1,1,1};
-        System.out.println(maxConsecBits(arr));
+        System.out.println(maxConsecBitwise(arr));
     }
 
     private static int maxConsecBits(int[] arr) {
@@ -12,6 +12,20 @@ public class MaxConsecutiveBits {
         int count= 1;
         for(int i=1; i<arr.length; i++) {
             if(arr[i] == arr[i-1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+        }
+
+        return count;
+    }
+
+    private static int maxConsecBitwise(int[] arr) {
+
+        int count= 1;
+        for(int i=1; i<arr.length; i++) {
+            if((arr[i] ^ arr[i-1]) == 0) {
                 count++;
             } else {
                 count = 1;
