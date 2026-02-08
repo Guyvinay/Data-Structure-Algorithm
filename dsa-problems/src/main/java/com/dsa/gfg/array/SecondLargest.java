d17140a099b383ae47f4bf94392b03f4ca5eec93
guyvinay <mrsinghvinay563@gmial.com>
2026-02-08 10:32:13 +0530

    thirdLargest

--- dsa-problems/src/main/java/com/dsa/gfg/array/SecondLargest.java
+++ dsa-problems/src/main/java/com/dsa/gfg/array/SecondLargest.java
@@ -1,32 +1,24 @@
-package com.dsa.gfg;
+package com.dsa.gfg.array;
 
-public class ThirdLargest {
+public class SecondLargest {
     public static void main(String[] args) {
         int[] arr = {1,2,4,6,7,9};
-        System.out.println(thirdLargest(arr));
+        System.out.println(getSecondLargest(arr));
     }
 
-    private static int thirdLargest(int[] arr) {
+    private static int getSecondLargest(int[] arr) {
         int largest = -1;
         int second = -1;
-        int third = -1;
         for(int i=0; i<arr.length; i++) {
             if(arr[i] > largest) {
-                third = second;
                 second = largest;
                 largest = arr[i];
             }
 
             if(arr[i] < largest && arr[i] > second) {
-                third = second;
                 second = arr[i];
             }
-
-            if(arr[i] < second && arr[i] > third) {
-                third = arr[i];
-            }
-
         }
-        return third;
+        return second;
     }
 }
--- dsa-problems/src/main/java/com/dsa/gfg/array/ThirdLargest.java
+++ dsa-problems/src/main/java/com/dsa/gfg/array/ThirdLargest.java
@@ -1,24 +1,32 @@
-package com.dsa.gfg;
+package com.dsa.gfg.array;
 
-public class SecondLargest {
+public class ThirdLargest {
     public static void main(String[] args) {
         int[] arr = {1,2,4,6,7,9};
-        System.out.println(getSecondLargest(arr));
+        System.out.println(thirdLargest(arr));
     }
 
-    private static int getSecondLargest(int[] arr) {
+    private static int thirdLargest(int[] arr) {
         int largest = -1;
         int second = -1;
+        int third = -1;
         for(int i=0; i<arr.length; i++) {
             if(arr[i] > largest) {
+                third = second;
                 second = largest;
                 largest = arr[i];
             }
 
             if(arr[i] < largest && arr[i] > second) {
+                third = second;
                 second = arr[i];
             }
+
+            if(arr[i] < second && arr[i] > third) {
+                third = arr[i];
+            }
+
         }
-        return second;
+        return third;
     }
 }
