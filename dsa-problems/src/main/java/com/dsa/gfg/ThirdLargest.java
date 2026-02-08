f55108f31472ff99d4ac616fcc140558e35130ef
guyvinay <mrsinghvinay563@gmial.com>
2026-02-08 10:03:28 +0530

    thirdLargest

---
+++ dsa-problems/src/main/java/com/dsa/gfg/ThirdLargest.java
@@ -0,0 +1,32 @@
+package com.dsa.gfg;
+
+public class ThirdLargest {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,6,7,9};
+        System.out.println(thirdLargest(arr));
+    }
+
+    private static int thirdLargest(int[] arr) {
+        int largest = -1;
+        int second = -1;
+        int third = -1;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] > largest) {
+                third = second;
+                second = largest;
+                largest = arr[i];
+            }
+
+            if(arr[i] < largest && arr[i] > second) {
+                third = second;
+                second = arr[i];
+            }
+
+            if(arr[i] < second && arr[i] > third) {
+                third = arr[i];
+            }
+
+        }
+        return third;
+    }
+}
