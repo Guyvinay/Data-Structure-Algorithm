f41e57198161adb46fd16efe3be4f0a8b622e983
guyvinay <mrsinghvinay563@gmail.com>
2026-02-14 00:22:42 +0530

    PairWithGivenSum

---
+++ dsa-problems/src/main/java/com/dsa/gfg/array/PairWithGivenSum.java
@@ -0,0 +1,16 @@
+package com.dsa.gfg.array;
+
+public class PairWithGivenSum {
+
+    public static void main(String[] args) {
+        int[] arr = {0, -1, 2, -3, 1};
+        int target = -2;
+
+        int start = 0;
+        int end = arr.length - 1;
+        for (int i=0; i<arr.length; i++) {
+            int middle = start + (end - start) / 2;
+        }
+    }
+
+}
